94ba2b217b7c267fa4bb35a9457c1ee8a0c68ff8
bobbysharma05 <https://github.com/bobbysharma05>
2025-03-25 21:52:12 +0530

    Code additions

--- lab7_8/ArchiveBox/Unique_CWE.py
+++ lab7_8/ArchiveBox/Unique_CWE.py
@@ -2,28 +2,21 @@ import os
 import json
 from collections import Counter
 
-
-def get_cwe(path):
-    cwe_cnt = Counter()
-    all_files = [i for i in os.listdir(path) if i.endswith(".json")]
-    for file in all_files:
-        full_path = os.path.join(path, file)
-        with open(full_path, 'r') as f:
+def generator(path):
+    cnt = Counter()
+    alls = [i for i in os.listdir(path) if i.endswith(".json")]
+    for f in alls:
+        f_path = os.path.join(path, f)
+        with open(f_path, 'r') as f:
             data = json.load(f)
 
             if 'results' in data and isinstance(data['results'], list):
                 for r in data['results']:
                     if 'issue_cwe' in r and 'id' in r['issue_cwe']:
-                        cwe_id = r['issue_cwe']['id']
-                        cwe_cnt[cwe_id] += 1
-
-    return dict(cwe_cnt)
+                        id = r['issue_cwe']['id']
+                        cnt[id] += 1
 
 
-
-
-if __name__ == "__main__":
-    ans = get_cwe("/home/bobby/Downloads/software/lab7-8/ArchiveBox/bandit_results")
-
-    for cwe_id, frequency in sorted(ans.items(), key=lambda x: x[1], reverse=True):
-        print(f"CWE-{cwe_id}: {frequency} occurrences")
+ans = generator("/home/bobby/Downloads/software/lab7-8/ArchiveBox/bandit_results")
+for id, freq in sorted(ans.items(), key=lambda x: x[1]):
+    print(f"CWE-{id}: {freq} frequency")
